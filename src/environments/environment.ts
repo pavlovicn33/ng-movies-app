cb856bdef9d13e1e0ee368fb4e0aad2ff4ba9cfa
Nikola Pavlovic <pavlovicn410@gmail.com>
2022-11-30 16:23:51 +0100

    chore:firebase initialized

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'movie-app-a619f',
+    appId: '1:218451149766:web:ac93629c83139405bde8c7',
+    storageBucket: 'movie-app-a619f.appspot.com',
+    apiKey: 'AIzaSyCaRbQ1yw7OrJDn2tFgIb0fnfIwcwf-d6s',
+    authDomain: 'movie-app-a619f.firebaseapp.com',
+    messagingSenderId: '218451149766',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
