b3309edbc059520cd94290c5495044cc15ba4e99
Nikola Pavlovic <pavlovicn410@gmail.com>
2022-12-27 02:08:01 +0100

    feature: email-handler-verification

--- src/app/pages/email-handler/email-handler.component.ts
+++ src/app/pages/email-handler/email-handler.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { environment } from 'src/environments/environment';
-import { verifyPasswordResetCode, confirmPasswordReset, checkActionCode, applyActionCode, sendPasswordResetEmail  } from "firebase/auth";
+import {
+  verifyPasswordResetCode,
+  confirmPasswordReset,
+  checkActionCode,
+  applyActionCode,
+  sendPasswordResetEmail,
+} from 'firebase/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-email-handler',
@@ -11,14 +19,13 @@ import { verifyPasswordResetCode, confirmPasswordReset, checkActionCode, applyAc
   styleUrls: ['./email-handler.component.scss'],
 })
 export class EmailHandlerComponent implements OnInit {
+  headerText: string = '';
 
-  headerText:string = ''
+  recoverEmailStatus: boolean = false;
+  verifyEmailStatus: boolean = false;
+  resetPasswordStatus: boolean = false;
 
-  recoverEmailStatus:boolean = false
-  verifyEmailStatus:boolean = false
-  resetPasswordStatus:boolean = false
-
-  constructor(private route: ActivatedRoute, private router:ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
@@ -26,69 +33,68 @@ export class EmailHandlerComponent implements OnInit {
       const actionCode = params['oobCode'];
       const lang = params['lang'] || 'en';
 
-      const config = {
-        apiKey: environment.firebase.apiKey,
-      };
-      const app = initializeApp(config);
-      const auth = getAuth(app);
-
-      switch (mode) {
-        case 'resetPassword':
-          this.resetPasswordStatus = true
-          this.headerText = "Reset Password"
-          this.handleResetPassword(auth, actionCode, lang);
-          break;
-        case 'recoverEmail':
-          this.recoverEmailStatus = true
-          this.headerText = "Recover your email"
-          this.handleRecoverEmail(auth, actionCode, lang);
-          break;
-        case 'verifyEmail':
-          this.verifyEmailStatus = true
-          this.headerText = "Verify your email"
-          this.handleVerifyEmail(auth, actionCode, lang);
-          break;
-        default:
+      if (firebase.apps.length === 0) {
+        const app = initializeApp(environment.firebase);
+        const auth = getAuth(app);
+        switch (mode) {
+          case 'resetPassword':
+            this.resetPasswordStatus = true;
+            this.headerText = 'Reset Password';
+            this.handleResetPassword(auth, actionCode, lang);
+            break;
+          case 'recoverEmail':
+            this.recoverEmailStatus = true;
+            this.headerText = 'Recover your email';
+            this.handleRecoverEmail(auth, actionCode, lang);
+            break;
+          case 'verifyEmail':
+            this.verifyEmailStatus = true;
+            this.headerText = 'Verify your email';
+            this.handleVerifyEmail(auth, actionCode, lang);
+            break;
+          default:
+        }
       }
     });
   }
 
-  handleResetPassword(auth:any, actionCode:any, lang:any) {
-    verifyPasswordResetCode(auth, actionCode).then((email) => {
-      const accountEmail = email;
-      const newPassword = "...";
-      confirmPasswordReset(auth, actionCode, newPassword).then((resp) => {
-
-      }).catch((error) => {
-
-      });
-    }).catch((error) => {
-
-    });
-  }
-  
-  handleRecoverEmail(auth:any, actionCode:any, lang:any) {
-    let restoredEmail:any = null;
-    checkActionCode(auth, actionCode).then((info) => {
-      restoredEmail = info['data']['email'];
-  
-      return applyActionCode(auth, actionCode);
-    }).then(() => {
-
-      sendPasswordResetEmail(auth, restoredEmail).then(() => {
-
-      }).catch((error) => {
-
-      });
-    }).catch((error) => {
-
-    });
+  handleResetPassword(auth: any, actionCode: any, lang: any) {
+    verifyPasswordResetCode(auth, actionCode)
+      .then((email) => {
+        const accountEmail = email;
+        const newPassword = '...';
+        confirmPasswordReset(auth, actionCode, newPassword)
+          .then((resp) => {})
+          .catch((error) => {});
+      })
+      .catch((error) => {});
   }
 
-  handleVerifyEmail(auth:any, actionCode:any, lang:any) {
-    applyActionCode(auth, actionCode).then((resp) => {
-    }).catch((error) => {
- 
-    });
+  handleRecoverEmail(auth: any, actionCode: any, lang: any) {
+    let restoredEmail: any = null;
+    checkActionCode(auth, actionCode)
+      .then((info) => {
+        restoredEmail = info['data']['email'];
+
+        return applyActionCode(auth, actionCode);
+      })
+      .then(() => {
+        sendPasswordResetEmail(auth, restoredEmail)
+          .then(() => {})
+          .catch((error) => {});
+      })
+      .catch((error) => {});
+  }
+
+  handleVerifyEmail(auth: any, actionCode: any, lang: any) {
+    applyActionCode(auth, actionCode)
+      .then((resp) => {
+        console.log('qweqew');
+        console.log(resp);
+      })
+      .catch((error) => {
+        console.log('qwerror');
+        console.log(error);
+      });
   }
 }
