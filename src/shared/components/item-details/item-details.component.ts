4611278191b3027cfa22ba2cef362c320e594af7
Nikola Pavlovic <pavlovicn410@gmail.com>
2022-12-28 18:08:11 +0100

    feature: added media-types

--- src/shared/components/item-details/item-details.component.ts
+++ src/shared/components/item-details/item-details.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from 'src/shared/services/movies/movies.service';
+import { ShowsService } from 'src/shared/services/shows/shows.service';
 
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.component.html',
-  styleUrls: ['./item-details.component.scss']
+  styleUrls: ['./item-details.component.scss'],
 })
-export class ItemDetailsComponent {
+export class ItemDetailsComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MoviesService,
+    private showService: ShowsService
+  ) {}
 
+  ngOnInit(): void {
+    this.route.params.subscribe((data: any) => {
+      let id = data['id'];
+      let media = data['media_type'];
+      if (media == 'tv') {
+        this.getShows(id);
+        return;
+      }
+      if (media == 'movie') {
+        this.getMovie(id);
+        return;
+      }
+      if (media == 'person') {
+        this.getPeople(id);
+        return;
+      }
+    });
+  }
+
+  getMovie(id: number) {
+    this.movieService.getMovieDetails(id).subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+  getShows(id: number) {
+    this.showService.getShowDetails(id).subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+  getPeople(id: number) {
+    this.movieService.getPeopleDetails(id).subscribe((data: any) => {
+      console.log(data);
+    });
+  }
 }
